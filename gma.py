842c3c1acbabfa6059e0bd68c23858ab00b4fe3b
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-27 13:43:39 +0530

    Update gma.py

--- gma.py
+++ gma.py
@@ -1,29 +1,28 @@
-import sys,string,math
-g1,g2=input().split()
-m1=len(g1)
-m2=len(g2)
-if m2>m1:
-    i=0
-    while i<m1 and g1[i]==g2[i]:
-        i+=1
-      print(m2-i)
-elif m2==m1:
-    i=0
-    while i<m2 and g1[i]==g2[i]:
-        i+=1
-     print(m2-i)
-else:
-    i=0
-    while i<m2 and s1[i]==s2[i]:
-        i+=1
-  g35=g1[i:]
-  g36=g2[i:]
-  lt=list(g34)
-  s=0
-for v in s34:
-    if v in lt:
-        s+=1
-        lt.remove(v)
- print(m1-1-v)   
-        
-    
+import sys, string, math
+da1,da2 = input().split()
+ms1 = len(da1)
+ms2 = len(da2)
+if ms2 > ms1 :
+    i = 0
+    while i<ms1 and da1[i] == da2[i] :
+        i += 1
+    print(ms2-i)
+elif ms2 == ms1 :
+    i = 0
+    while i<ms2 and da1[i] == da2[i] :
+        i += 1
+    print(ms2-i)
+else :
+
+    i = 0
+    while i<ms2 and da1[i] == da2[i] :
+        i += 1
+    da31 = da1[i:]
+    da32 = da2[i:]
+    gt = list(da31)
+    gm = 0
+    for cn in da32 :
+        if cn in gt :
+            gm += 1
+            gt.remove(cn)
+    print(ms1-i-gm)
